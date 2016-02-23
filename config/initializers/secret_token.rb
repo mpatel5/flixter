f6a30f78c339807ad3cf326ee785e82ff3cf420f
Mitesh Patel <mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.name Mitesh>
2016-02-23 01:51:28 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flixter::Application.config.secret_key_base = '7a573924655d650f5e82017769b8c890c0d8466a23a86795f0d018f6adc03f0e70fc6fb8fb964fd268cd3fea35362fade51a2f73b7a43343ce9c950e71e47449'
