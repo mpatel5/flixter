4c46486acd86371fc9d19f1cca9b7258fa04756b
Mitesh Patel <mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.email mpatel5@icloud.comgit config --global user.name Mitesh>
2016-03-30 19:29:58 +0000

    Added a Static Page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,18 @@
 class StaticPagesController < ApplicationController
   def index
+    @courses = Course.all
+    
+  end
+
+  def privacy
+
+  end
+
+  def team
+
+  end
+
+  def careers
 
   end
 end
